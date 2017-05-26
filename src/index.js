f6f992f4328bb4121221c5ef42b8ebd5b1fd3920
Denis <Denchique@gmail.com>
2017-05-26 14:12:59 +0300

    test app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
+var os = require("os");
 import React from "react";
 import { render } from "react-dom";
 
+
+
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+
 render(
-		<h1>Hello react</h1>,
-	document.getElementById('root')
-
+  <Clock/>,
+  document.getElementById('root')
 );
-
-
-
-
